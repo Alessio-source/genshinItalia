abe453c382e2e02ab1c6d723bf4060576780cc6f
Alessio-source <alessiop2801@gmail.com>
2021-03-28 23:33:52 +0200

    Prime modifiche

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+        <title>GenshinItalia - Homepage</title>
+    </head>
+    <body>
+        <header>
+            <a href="{{ url('/') }}">Genshin Italia</a>
+
+            <ul>
+                <li><a href="">Prova</a></li>
+                <li><a href="">Prova</a></li>
+                <li><a href="">Prova</a></li>
+                <li><a href="">Prova</a></li>
+            </ul>
+        </header>
+
+        <main>
+            <div class="container">
+                <h1>Ciao</h1>
+            </div>
+        </main>
+
+        <footer>
+
+        </footer>
+    </body>
+</html>
