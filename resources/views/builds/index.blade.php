d65632f2fe7807e7fe2b34154be2ef1753a8bb18
Alessio-source <alessiop2801@gmail.com>
2021-04-14 19:06:33 +0200

    Header e piccole sistemazioni in build

--- resources/views/builds/index.blade.php
+++ resources/views/builds/index.blade.php
@@ -5,12 +5,35 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
         <title>GenshinItalia - Builds</title>
 
     </head>
     <body class="build_index">
+
+        <header>
+            <div class="container">
+                <section class="right">
+                    <img src="{{ asset('images/Paimon.png') }}" alt="">
+                    <a href="{{ url('/') }}">Genshin Italia</a>
+                </section>
+    
+                <nav>
+                    <ul>
+                        <li><a href="{{ url('/') }}">Homepage</a></li>
+                        <li><a href="">News</a></li>
+                        <li><a href="">Chi siamo?</a></li>
+                        <li><a href="">Contatti</a></li>
+                        @auth
+                        <li><a href="{{ route('dashboard.index') }}">Pannello di controllo</a></li>
+                        @endauth
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
         <main>
             <div class="cards">
                 @foreach ($characters as $character)
